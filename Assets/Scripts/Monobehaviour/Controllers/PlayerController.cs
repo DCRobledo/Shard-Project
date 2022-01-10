48de4e2b60cb73ea91c2e4fc20483979b9c2ceec
DCRobledo <daniel.canorobledo@gmail.com>
2022-01-10 19:47:32 +0100

    Set up the new input system framework

---
+++ Assets/Scripts/Monobehaviour/Controllers/PlayerController.cs
@@ -0,0 +1,43 @@
+using Shard.Assets.Scripts.Entities;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Shard.Assets.Scripts.Monobehaviour.Controllers 
+{
+    public class PlayerController : MonoBehaviour
+    {
+        private PlayerInputActions playerInputActions;
+        private InputAction movement;
+
+
+        private void Awake() {
+            playerInputActions = new PlayerInputActions();
+        }
+
+        private void OnEnable() {
+            movement = playerInputActions.Player.Movement;
+            movement.Enable();
+
+            playerInputActions.Player.Jump.performed += Jump;
+            playerInputActions.Player.Jump.Enable();
+        }
+
+        private void OnDisable() {
+            movement.Disable();
+            playerInputActions.Player.Jump.Disable();
+        }
+
+        private void FixedUpdate() {
+            Debug.Log("Movement values: " + movement.ReadValue<Vector2>());
+        }
+
+
+        private void Jump(InputAction.CallbackContext obj) {
+            Debug.Log("Jump!");
+        }
+    }
+}
+
+
