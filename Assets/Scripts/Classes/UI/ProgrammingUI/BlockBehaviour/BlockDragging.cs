5a8b8df0dcf408c277a332f7c090dc73f64d01f2
DCRobledo <daniel.canorobledo@gmail.com>
2022-02-04 17:11:13 +0100

    Implemented generation of new block on drag

--- Assets/Scripts/Classes/UI/ProgrammingUI/BlockBehaviour/BlockDragging.cs
+++ Assets/Scripts/Classes/UI/ProgrammingUI/BlockBehaviour/BlockDragging.cs
@@ -5,22 +5,66 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class BlockDragging : DragAndDrop, IBeginDragHandler, IEndDragHandler
+public class BlockDragging : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
 {
-    private GameObject block;
+    [SerializeField]
+    private GameObject draggableBlockPrefab;
 
-    private new void Awake() {
-        base.Awake();
-        block = this.transform.GetChild(0).gameObject;
+    [SerializeField]
+    private float dragSpeed = .05f;
+
+    private Vector3 velocity = Vector3.zero;
+
+    private GameObject draggableBlock;
+    private RectTransform draggableBlockRectTransform;
+
+    private GameObject blockImage;
+
+    private void Awake() {
+        blockImage = this.transform.GetChild(0).gameObject;
+    }
+
+    public void OnDrag(PointerEventData eventData) {
+        if(RectTransformUtility.ScreenPointToWorldPointInRectangle(
+            draggableBlockRectTransform,
+            eventData.position,
+            eventData.pressEventCamera,
+            out var globalMousePosition
+        )) {
+            draggableBlockRectTransform.position =
+            Vector3.SmoothDamp(
+                draggableBlockRectTransform.position,
+                globalMousePosition,
+                ref velocity,
+                dragSpeed
+            );
+        }
     }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-        VisualUtils.ChangeImageAlpha(ref block, -.7f);
+        // Enter dragging state
+        VisualUtils.ChangeImageAlpha(ref blockImage, -.7f);
+
+        // Create the draggable block
+        CreateDraggableBlock();
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        VisualUtils.ChangeImageAlpha(ref block, .7f);
+        // End dragging state
+        VisualUtils.ChangeImageAlpha(ref blockImage, .7f);
+
+        // Place the draggable block
+        Destroy(draggableBlock.gameObject);
+    }
+
+    private void CreateDraggableBlock(){
+        draggableBlock = Instantiate(draggableBlockPrefab);
+        draggableBlockRectTransform = draggableBlock.GetComponent<RectTransform>();
+
+        draggableBlock.transform.SetParent(GameObject.Find("blocks").transform, false);
+        draggableBlock.transform.position = this.transform.position;
+        
     }
 }
