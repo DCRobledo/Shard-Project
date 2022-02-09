e8ab151974efa7753215717f4edbc9e0dfcd570e
DCRobledo <daniel.canorobledo@gmail.com>
2022-02-09 17:57:56 +0100

    Refactored indentation management

--- Assets/Scripts/Classes/UI/ProgrammingUI/BlockBehaviour/BlockManagement.cs
+++ Assets/Scripts/Classes/UI/ProgrammingUI/BlockBehaviour/BlockManagement.cs
@@ -37,53 +37,9 @@ namespace Shard.UI.ProgrammingUI
                     block.GetComponent<RectTransform>(),
                     indentation
                 );
-                
-                //UpdateBlockPosition(blockSpace - 1);
             }
 
             else Destroy(block.gameObject);
-
-            //UpdateIndentations();
-        }
-
-        private void UpdateIndentations() {
-            // Check all block spaces
-            for (int i = 0; i < this.transform.childCount - 1; i++)
-            {
-                Transform blockSpaceTransform = GetBlockParent(i).transform;
-                BlockSpace blockSpace = blockSpaceTransform.gameObject.GetComponent<BlockSpace>();
-
-                // Check if there is a block within each block space 
-                if(IsThereBlock(i)) {
-                    BehaviourBlock behaviourBlock = GetBlock(i).GetComponent<BehaviourBlock>();
-
-                    // For conditional blocks, we indent the very next block
-                    if(behaviourBlock.blockType == BehaviourBlock.BlockType.IF || behaviourBlock.blockType == BehaviourBlock.BlockType.ELSE || behaviourBlock.blockType == BehaviourBlock.BlockType.ELSEIF) {
-                        BlockSpace nextBlockSpace = GetBlockParent(i + 1).GetComponent<BlockSpace>();
-
-                        // Only modify indentation if needed
-                        if(blockSpace.GetIndentation() == nextBlockSpace.GetIndentation()) {
-                            int indentationModifier = blockSpace.GetIndentation() - nextBlockSpace.GetIndentation() + 1;
-
-                            nextBlockSpace.ModifyIndentation(indentationModifier);
-
-                            // Update block if needed
-                            if(IsThereBlock(i + 1))
-                                UpdateBlockPosition(i + 1);
-                        }
-                    }
-                }
-            }
-        }
-
-        private void UpdateBlockPosition(int index) {
-            GameObject block = GetBlock(index);
-
-            block.GetComponent<RectTransform>().anchoredPosition = CalculateBlockPosition(
-                GetBlockContainer(index).GetComponent<RectTransform>(),
-                block.GetComponent<RectTransform>(),
-                GetBlockParent(index).GetComponent<BlockSpace>().GetIndentation()
-            );
         }
 
         private Vector3 CalculateBlockPosition(RectTransform blockContainer, RectTransform block, float indentation) {
--- Assets/Scripts/Classes/UI/ProgrammingUI/BlockBehaviour/BlockSpace.cs
+++ Assets/Scripts/Classes/UI/ProgrammingUI/BlockBehaviour/BlockSpace.cs
@@ -14,27 +14,14 @@ namespace Shard.UI.ProgrammingUI
         public bool isInScrollArea;
 
 
-        private void Awake() {
-            //ResetIndentation();
-        }
-
-
         private void OnTriggerEnter2D(Collider2D other) {
             isInScrollArea = other.transform.tag == "Behaviour Block" ? isInScrollArea : other.transform.tag == "Scrollable Block Area";
         }
 
-        public bool CanBeUsed() { return isInScrollArea; }
 
-        public void ResetIndentation()
-        {
-            this.indentation = 1;
+        public bool CanBeUsed() {
+            return isInScrollArea; 
         }
-        
-        public void ModifyIndentation(int modifier)
-        {
-            this.indentation += modifier;
-        }
-    
 
         public int GetIndex()
         {
