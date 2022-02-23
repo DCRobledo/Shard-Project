ee56814fced258f95b0cb4932ffbea70c3cc794d
DCRobledo <daniel.canorobledo@gmail.com>
2022-02-23 17:33:47 +0100

    Namespace fixing

--- Assets/Scripts/Classes/Entities/Player/PlayerMovement.cs
+++ Assets/Scripts/Classes/Entities/Player/PlayerMovement.cs
@@ -3,26 +3,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : EntityMovement
+namespace Shard.Entities
 {
-    public override void Move(float x, float y) 
+    public class PlayerMovement : EntityMovement
     {
-        // Compute the new target velocity, smooth it, and apply it
-        Vector3 targetVelocity = new Vector2(Mathf.Round(x) * speed, rigidBody.velocity.y);
-        rigidBody.velocity = Vector3.SmoothDamp(rigidBody.velocity, targetVelocity, ref velocity, smoothingFactor);
+        public override void Move(float x, float y) 
+        {
+            // Compute the new target velocity, smooth it, and apply it
+            Vector3 targetVelocity = new Vector2(Mathf.Round(x) * speed, rigidBody.velocity.y);
+            rigidBody.velocity = Vector3.SmoothDamp(rigidBody.velocity, targetVelocity, ref velocity, smoothingFactor);
 
-        // Flip if necessary
-        if      (x > 0 && this.gameObject.transform.localScale.x < 0) Flip();
-        else if (x < 0 && this.gameObject.transform.localScale.x > 0) Flip();
+            // Flip if necessary
+            if      (x > 0 && this.gameObject.transform.localScale.x < 0) Flip();
+            else if (x < 0 && this.gameObject.transform.localScale.x > 0) Flip();
+        }
+
+        public override void Jump(bool jump) 
+        {
+            // Realising the jump button is always allowed
+            if(!jump) this.shouldJump = jump;
+
+            // But we want to delay the jumps between one another
+            else if(jump && canJump) 
+                this.shouldJump = jump;
+        } 
     }
-
-    public override void Jump(bool jump) 
-    {
-        // Realising the jump button is always allowed
-        if(!jump) this.shouldJump = jump;
-
-        // But we want to delay the jumps between one another
-        else if(jump && canJump) 
-            this.shouldJump = jump;
-    } 
 }
--- Assets/Scripts/Classes/Entities/Robot/RobotMovement.cs
+++ Assets/Scripts/Classes/Entities/Robot/RobotMovement.cs
@@ -1,24 +1,26 @@
-using Shard.Entities;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RobotMovement : EntityMovement
+namespace Shard.Entities
 {
-    public override void Move() { 
-        float x = isFacingRight ? 1 : -1;
+    public class RobotMovement : EntityMovement
+    {
+        public override void Move() { 
+            float x = isFacingRight ? 1 : -1;
 
-        // Compute the new target velocity, smooth it, and apply it
-        Vector3 targetVelocity = new Vector2(Mathf.Round(x) * speed, rigidBody.velocity.y);
-        rigidBody.velocity = Vector3.SmoothDamp(rigidBody.velocity, targetVelocity, ref velocity, smoothingFactor);
+            // Compute the new target velocity, smooth it, and apply it
+            Vector3 targetVelocity = new Vector2(Mathf.Round(x) * speed, rigidBody.velocity.y);
+            rigidBody.velocity = Vector3.SmoothDamp(rigidBody.velocity, targetVelocity, ref velocity, smoothingFactor);
 
-        // Flip if necessary
-        if      (x > 0 && this.gameObject.transform.localScale.x < 0) Flip();
-        else if (x < 0 && this.gameObject.transform.localScale.x > 0) Flip();
+            // Flip if necessary
+            if      (x > 0 && this.gameObject.transform.localScale.x < 0) Flip();
+            else if (x < 0 && this.gameObject.transform.localScale.x > 0) Flip();
+        }
+
+        public override void Jump()  {
+            if(canJump)
+                this.shouldJump = true;
+        } 
     }
-
-    public override void Jump()  {
-        if(canJump)
-            this.shouldJump = true;
-    } 
 }
