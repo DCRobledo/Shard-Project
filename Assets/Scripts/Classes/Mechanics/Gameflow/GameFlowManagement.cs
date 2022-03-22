16cab3479b44a48508411b4e781e5572898fc39b
DCRobledo <daniel.canorobledo@gmail.com>
2022-03-22 09:48:35 +0100

    Implemented level end flow

--- Assets/Scripts/Classes/Mechanics/Gameflow/GameFlowManagement.cs
+++ Assets/Scripts/Classes/Mechanics/Gameflow/GameFlowManagement.cs
@@ -2,19 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameFlowManagement : MonoBehaviour
+namespace Shard.Gameflow
 {
-    [SerializeField]
-    private bool playStartAnimation = true;
+    public class GameFlowManagement : MonoBehaviour
+    {
+        [SerializeField]
+        private bool playStartAnimation = true;
 
-    [SerializeField]
-    private GameObject blackPanel;
-    private Animator animator;
+        [SerializeField]
+        private GameObject blackPanel;
+        private Animator animator;
 
 
-    private void Awake() {
-        this.animator = blackPanel.GetComponent<Animator>();
+        private void Awake() {
+            this.animator = blackPanel.GetComponent<Animator>();
 
-        if (!playStartAnimation) animator.Play("idle_transparent");
+            if (!playStartAnimation) animator.Play("idle_transparent");
+        }
+
+        private void OnEnable() {
+            WinTrigger.endLevelEvent += EndLevel;
+        }
+
+        private void OnDisable() {
+            WinTrigger.endLevelEvent -= EndLevel;
+        }
+
+
+        private void EndLevel() {
+            animator.SetTrigger("endLevel");
+        }
     }
 }
+
+
