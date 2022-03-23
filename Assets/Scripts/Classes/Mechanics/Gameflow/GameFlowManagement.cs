08f1b5a632d3beb0738873666ab9e0f724cf6e8b
DCRobledo <daniel.canorobledo@gmail.com>
2022-03-23 17:43:15 +0100

    Implemented end door animation

--- Assets/Scripts/Classes/Mechanics/Gameflow/GameFlowManagement.cs
+++ Assets/Scripts/Classes/Mechanics/Gameflow/GameFlowManagement.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Tilemaps;
 
 namespace Shard.Gameflow
 {
@@ -12,17 +13,24 @@ namespace Shard.Gameflow
         private bool playStartAnimation = true;
 
         [SerializeField]
-        private GameObject blackPanel;
-        private Animator animator;
+        private GameObject levelTransitionGO;
+        private Animator levelTransitionAnimator;
+
+        [SerializeField]
+        private GameObject endDoor;
+        private Animator endDoorAnimator;
 
         [SerializeField]
         private GameObject winTrigger;
 
 
         private void Awake() {
-            this.animator = blackPanel.GetComponent<Animator>();
+            this.levelTransitionAnimator = levelTransitionGO.GetComponent<Animator>();
+            this.endDoorAnimator         = endDoor.GetComponent<Animator>();
 
-            if (!playStartAnimation) animator.Play("idle_transparent");
+            if (!playStartAnimation) levelTransitionAnimator.Play("idle_transparent");
+
+            winTrigger.GetComponent<TilemapRenderer>().enabled = false;
         }
 
         private void Start() {
@@ -40,7 +48,7 @@ namespace Shard.Gameflow
 
         private IEnumerator StartLevel() {
             // Wait for start animation to finish
-            yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
+            yield return new WaitForSeconds(levelTransitionAnimator.GetCurrentAnimatorStateInfo(0).length);
 
             // Activate player input
             PlayerController.Instance.EnableInput();
@@ -55,19 +63,33 @@ namespace Shard.Gameflow
             PlayerController.Instance.DisableInput();
 
             // Start end level sequence
-            StartCoroutine(AfterEndLevel());
+            StartCoroutine(EndLevelSequence());
         }
 
-        private IEnumerator AfterEndLevel() {
-            // Play end animation
-            animator.SetTrigger("endLevel");
+        private IEnumerator EndLevelSequence() {
+            // Play first half of fade out animation
 
-            // Wait for end animation to finish
-            yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
+            // Open end door
+            endDoorAnimator.SetTrigger("open");
+            //yield return new WaitForSeconds(endDoorAnimator.GetCurrentAnimatorClipInfo(0)[0].clip.averageDuration);
+
+            // Walk player into door
+
+            // Fade out player
+
+            // Close end door
+            endDoorAnimator.SetTrigger("close");
+            //yield return new WaitForSeconds(endDoorAnimator.GetCurrentAnimatorClipInfo(0)[0].clip.averageDuration);
+
+            // Play second half of fade out animation
+            levelTransitionAnimator.SetTrigger("endLevel");
 
             // Load next level
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+            yield return null;
         }
+
     }
 }
 
