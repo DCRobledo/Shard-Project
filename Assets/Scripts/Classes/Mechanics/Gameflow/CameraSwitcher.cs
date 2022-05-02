8c474e37a0a485a3babc7cd4262c458cc021be75
DCRobledo <daniel.canorobledo@gmail.com>
2022-05-02 10:39:47 +0200

    Prevented camera from following fall

--- Assets/Scripts/Classes/Mechanics/Gameflow/CameraSwitcher.cs
+++ Assets/Scripts/Classes/Mechanics/Gameflow/CameraSwitcher.cs
@@ -15,14 +15,42 @@ namespace Shard.Gameflow
     
         private static Animator cameraAnimator;
 
+        private Coroutine cameraCoolDown;
+
 
         private void Awake() {
             cameraAnimator = stateDrivenCamera.GetComponent<Animator>();
         }
 
+        private void OnEnable() {
+            DeathTrigger.deathTriggerEvent += PreventCameraFollow;
+        }
+
+        private void OnDisable() {
+            DeathTrigger.deathTriggerEvent -= PreventCameraFollow;
+        }
+
 
         public static void SwitchCamera(CameraEnum.VirtualCamera targetCamera) {
             cameraAnimator.Play(targetCamera.ToString().ToLower());
+        }
+
+        public void PreventCameraFollow(string entity) {
+            if(entity == "Player" && cameraCoolDown == null) {
+                GameObject activeCameraGO = stateDrivenCamera.GetComponent<CinemachineStateDrivenCamera>().LiveChild.VirtualCameraGameObject;
+                CinemachineVirtualCamera activeCamera = activeCameraGO.GetComponent<CinemachineVirtualCamera>();
+                Transform follow = activeCamera.Follow;
+
+                cameraCoolDown = StartCoroutine(CameraFollowCoolDown(activeCamera, follow));
+            }
+        }
+
+        private IEnumerator CameraFollowCoolDown(CinemachineVirtualCamera camera, Transform follow) {
+            camera.Follow = null;
+
+            yield return new WaitForSeconds(1f);
+
+            camera.Follow = follow;
         }
     }    
 }
